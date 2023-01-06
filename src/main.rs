bb73a82d3f1681e6bab9b95f17a3e567903f69bb
PeterTiny1 <56882034+PeterTiny1@users.noreply.github.com>
2023-01-06 21:54:28 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -854,7 +854,8 @@ fn main() {
         }
     }
     if save {
-        for (location, data) in state.generated_chunkdata.lock().unwrap().iter() {
+        let generated_chunkdata = state.generated_chunkdata.lock().unwrap();
+        for (location, data) in generated_chunkdata.iter() {
             let location = format!("{}.bin", location.iter().join(","));
             let path = Path::new(&location);
             if let Ok(mut file) = File::create(path) {
